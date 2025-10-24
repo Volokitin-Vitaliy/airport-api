77468d0a50c3140dccfb05e4635610a6250388ef
Fibonacci <volokitin.vitaliy@icloud.com>
2025-10-24 07:42:05 +0300

    Register airport models in Django admin

--- airport/admin.py
+++ airport/admin.py
@@ -1,3 +1,49 @@
 from django.contrib import admin
+from .models import Airport, Route, AirplaneType, Airplane, Crew, Flight, Order, Ticket
 
-# Register your models here.
+
+@admin.register(Airport)
+class AirportAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "closest_big_city")
+    search_fields = ("name", "closest_big_city")
+
+
+@admin.register(Route)
+class RouteAdmin(admin.ModelAdmin):
+    list_display = ("id", "source", "destination", "distance")
+    list_filter = ("source", "destination")
+
+
+@admin.register(AirplaneType)
+class AirplaneTypeAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+    )
+
+
+@admin.register(Airplane)
+class AirplaneAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "airplane_type", "rows", "seats_in_row")
+
+
+@admin.register(Crew)
+class CrewAdmin(admin.ModelAdmin):
+    list_display = ("id", "first_name", "last_name")
+
+
+@admin.register(Flight)
+class FlightAdmin(admin.ModelAdmin):
+    list_display = ("id", "route", "airplane", "departure_time", "arrival_time")
+    filter_horizontal = ("crew",)
+    date_hierarchy = "departure_time"
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "created_at")
+
+
+@admin.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ("id", "flight", "order", "row", "seat")
