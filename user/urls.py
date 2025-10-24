d6aed8326c916e07ccf1d0441ffb2af6e0e191c3
Fibonacci <volokitin.vitaliy@icloud.com>
2025-10-24 08:02:07 +0300

    Add URL routes for user registration and JWT authentication

---
+++ user/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from rest_framework_simplejwt.views import (TokenObtainPairView,
+                                            TokenRefreshView, TokenVerifyView)
+
+from user.views import CreateUserView, ManageUserView, UserApiRootView
+
+app_name = "user"
+
+urlpatterns = [
+    path("", UserApiRootView.as_view(), name="api-root"),
+    path("register/", CreateUserView.as_view(), name="create"),
+    path("me/", ManageUserView.as_view(), name="manage"),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+]
